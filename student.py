96d10dfbd16eca5347839236dc337e9a86d7d745
vishal8202 <111052930+vishal8202@users.noreply.github.com>
2022-11-03 02:00:25 +0530

    display

--- student.py
+++ student.py
@@ -10,7 +10,11 @@ while True:
     print('3 search a student')
     print('4 update the student')
     print('5 delete a student')
-    print('6 exit')
+    print('6 Add marks to subjects')
+    print('7 Total mark obtained by each student')
+    print('8 Subject wise mark')
+    print('9 Individual mark for each user')
+    print('10 exit')
 
     choice = int(input('Enter an option: '))
     if(choice==1):
@@ -32,13 +36,24 @@ while True:
             print(i)
     elif(choice==3):
         print('search a student')
+
         adm = input('enter the admi number u need : ')
         sql = 'SELECT `id`, `name`, `admno`, `rollno`, `collage` FROM `students` WHERE `admno`=' +adm
+
         mycursor.execute(sql)
         result = mycursor.fetchall()
         print(result)
     elif(choice==4):
         print('update the student')
+        admo = input('enter the adminnumber to be updated : ')
+        name = input('enter the name for the update: ')
+        #admo = input('enter the adminnumber')
+        rollno = input('enter the roll no to be updated : ')
+        college = input('enter the college name to be updated : ')
+        sql = "UPDATE `students` SET `name`='"+name+"',`admno`='"+admo+"',`rollno`='"+rollno+"',`collage`='"+college+"' WHERE `admno`="+admo
+        mycursor.execute(sql)
+        mydb.commit()
+        print('Succesfully updated !!!')
     elif(choice==5):
         print('delete the student')
         admo = input('Enter the admin number to be deleting : ')
@@ -46,5 +61,36 @@ while True:
         mycursor.execute(sql)
         mydb.commit()
         print('Deleted succesfully')
-    elif(choice==6):
+    elif(choice == 6 ):
+        print('Adding mark')
+        
+        # studentid = input('Enter the student id to which student the mark is inserting : ')
+        adm = input('enter the admi number u need : ')
+        sql = 'SELECT `id` FROM `students` WHERE `admno`=' +adm
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        id = 0
+        for i in result:
+            id = str(i[0])
+        print('Id of the student : ', id)
+        phy_mark = input('Enter the Physics mark : ')
+        chem_mark = input('Enter the chemistry mark : ')
+        math_mark = input('Enter the maths mark : ')
+        sql = 'INSERT INTO `marks`(`Student_id`, `physics_mark`, `chemistry_mark`, `maths_mark`) VALUES (%s,%s,%s,%s)'
+        data = (id,phy_mark,chem_mark,math_mark)
+        mycursor.execute(sql,data)
+        mydb.commit()
+    elif(choice == 7):
+        print('Display the marks of each students')
+        sql = 'SELECT `Student_id`,`physics_mark`, `chemistry_mark`, `maths_mark` FROM `marks`'
+        mycursor.execute(sql)
+        result = mycursor.fetchall()
+        for i in result:
+            print(i)
+        
+    elif(choice==8):
+        print()
+    elif(choice==9):
+        print()
+    elif(choice==10):
         break
